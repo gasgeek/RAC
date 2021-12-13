dc3fe3d1b2906f144f7ed100a95ed2d3ab77b4be
gasgeek <94107605+gasgeek@users.noreply.github.com>
2021-12-13 23:38:42 +0300

    2

--- src/com/company/Input.java
+++ src/com/company/Input.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Input {
 
     private static String input1, operation, input3;
@@ -39,10 +38,10 @@ public class Input {
                     if ( Input.isRoman1() && Converter.romanToArabic(Input.getInput1()) > 10 ) {
                         throw new Exception("Input number is out of range");
                     } else {
-                        if ( ((Input.getInput1().contains("0")) && Input.getInput1().length() <= 2) ) {
+                        if ( ((Input.getInput1().contains("0")) && Input.getInput1().length() > 2) ) {
                             throw new Exception("Only positive integers are allowed");
                         } else {
-                            if ( ((Input.getInput3().contains("0")) && Input.getInput3().length() <= 3) ) {
+                            if ( ((Input.getInput3().contains("0")) && Input.getInput3().length() > 2) ) {
                                 throw new Exception("Only positive integers are allowed");
                             } else {
                                 if ( (Input.getInput1().contains("-")) || (Input.getInput3().contains("-")) ) {
@@ -60,71 +59,70 @@ public class Input {
         }
     }
 
-            public void getInput() throws Exception {
+    public void getInput() throws Exception {
 
-                BufferedReader BufferedInputData = new BufferedReader(new InputStreamReader(System.in));
-                System.out.println("Введите число1, требуемую арифметическую операцию и число2, разделяя их пробелами.");
+        BufferedReader BufferedInputData = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите число1, требуемую арифметическую операцию и число2, разделяя их пробелами.");
 
-                String[] data;
-                try {
-                    String inputString = BufferedInputData.readLine().toUpperCase();
-                    /* *The minimum number of character is  : 1_+_1 = 5
-                     *The maximum number of character is: VIII_+_VIII = 11   */
-                    if ( (inputString.length() < 5) || (inputString.length() > 11) ) {
-                        throw new Exception("Invalid input. Input is not an arithmetical expression.");
-                    }
-                    // Let's check input for possible illegal symbols
-                    if ( inputString.contains(",") || inputString.contains((".")) ) {
-                        throw new IllegalArgumentException("We can operate with integers only..");
-                    }
-                    // Let's check there is only one arithmetic operation
+        String[] data;
+        try {
+            String inputString = BufferedInputData.readLine().toUpperCase();
+            /* *The minimum number of character is  : 1_+_1 = 5
+             *The maximum number of character is: VIII_+_VIII = 11   */
+            if ( (inputString.length() < 5) || (inputString.length() > 11) ) {
+                throw new Exception("Invalid input. Input is not an arithmetical expression.");
+            }
+            // Let's check input for possible illegal symbols
+            if ( inputString.contains(",") || inputString.contains((".")) ) {
+                throw new IllegalArgumentException("We can operate with integers only..");
+            }
+            // Let's check there is only one arithmetic operation
 
-                    int cont = 0;
-                    for (int i = 0; i < inputString.length() - 1; i++) {
-                        if ( inputString.charAt(i) == ' ' ) {
-                            cont++;
-                        }
-                    }
-                    if ( cont > 2 ) {
-                        throw new IllegalArgumentException("Only one arithmetic operation is allowed");
-                    }
-
-                    data = inputString.split("\\s+");
-                    input1 = data[0];
-                    operation = data[1];
-                    input3 = data[2];
-                } catch (
-                        IOException a) {
-                    a.printStackTrace();
+            int cont = 0;
+            for (int i = 0; i < inputString.length() - 1; i++) {
+                if ( inputString.charAt(i) == ' ' ) {
+                    cont++;
                 }
             }
-
-            public static String getInput1 () {
-                return input1;
+            if ( cont > 2 ) {
+                throw new IllegalArgumentException("Only one arithmetic operation is allowed");
             }
 
-            public static String getOperation () {
-                return operation;
-            }
-
-            public static String getInput3 () {
-                return input3;
-            }
-
-
-            public static boolean _isArabic1 () {
-                return Character.isDigit(Input.getInput1().charAt(0));
-            }
-
-            public static boolean _isArabic2 () {
-                return Character.isDigit(Input.getInput3().charAt(0));
-            }
-
-            private static boolean _isRoman1 () {
-                return Input.getInput1().contains("I") || Input.getInput1().contains("V") || Input.getInput1().contains("X");
-            }
-
-            private static boolean _isRoman2 () {
-                return Input.getInput3().contains("I") || Input.getInput3().contains("V") || Input.getInput3().contains("X");
-            }
+            data = inputString.split("\\s+");
+            input1 = data[0];
+            operation = data[1];
+            input3 = data[2];
+        } catch (
+                IOException a) {
+            a.printStackTrace();
         }
+    }
+
+    public static String getInput1() {
+        return input1;
+    }
+
+    public static String getOperation() {
+        return operation;
+    }
+
+    public static String getInput3() {
+        return input3;
+    }
+
+    public static boolean _isArabic1() {
+        return Character.isDigit(Input.getInput1().charAt(0));
+    }
+
+    public static boolean _isArabic2() {
+        return Character.isDigit(Input.getInput3().charAt(0));
+    }
+
+    private static boolean _isRoman1() {
+        return Input.getInput1().contains("I") || Input.getInput1().contains("V") || Input.getInput1().contains("X");
+    }
+
+    private static boolean _isRoman2() {
+        return Input.getInput3().contains("I") || Input.getInput3().contains("V") || Input.getInput3().contains("X");
+    }
+}
